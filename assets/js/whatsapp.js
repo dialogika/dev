6955d7f12d7f9dd754edda956478b90d5bdc7bb5
Dialogika <dialogikadunia@gmail.com>
2021-09-22 16:51:59 +0700

    bismillah

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -1,10 +1,10 @@
-$(document).on('click','.send_form', function(){
+$(document).on('click','.send_contact', function(){
     var input_blanter = document.getElementById('names');
 
     /* Whatsapp Settings */
     var walink = 'https://web.whatsapp.com/send',
         phone = '6282137760669',
-        walink2 = 'Salam Hangat, Saya melihat *Website Dialogika* dan saya tertarik untuk tahu lebih lanjut.' + '%0A %0A' 
+        walink2 = 'Salam Hangat, Saya melihat *Website Dialogika* dan saya tertarik untuk tahu lebih lanjut.' + '%0A%0A' 
                 + 'Sebelumnya perkenalkan saya: ',
         text_yes = 'Terkirim.',
         text_no = 'Isi semua Formulir lalu klik Send.';
@@ -17,21 +17,19 @@ $(document).on('click','.send_form', function(){
     if("" != input_blanter.value){
 
      /* Call Input Form */
-    var input_programs = $("#programs :selected").text(), 
-        input_names = $("#names").val(),
-        input_emails = $("#emails").val(),
-        input_works = $("#works").val(),
-        input_locations = $("#locaitions").val(),
-        input_ages = $("#ages").val(),
-        input_messages = $("#messages").val();
+    var input_name2     = $("#names").val(),
+        input_domisili2 = $("#locations").val(),
+        input_job2      = $("#works").val(),
+        input_program2  = $("#programs :selected").text()
+        input_message2  = $("#messages").val();
 
     /* Final Whatsapp URL */
     var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + walink2 + '%0A%0A' +
-        '*Name* : ' + input_names + '%0A' +
-        '*Program* : ' + input_programs + '%0A' +
-        '*Job* : ' + input_jobs + '%0A' +
-        '*Domisili* : ' + input_locations + '%0A' +
-        '*Isi Message* : ' + input_messages + '%0A';
+        '*Name*         : ' + input_name2 + '%0A' +
+        '*Domisili*     : ' + input_domisili2 + '%0A' +
+        '*Program*      : ' + input_program2 + '%0A' +
+        '*Perkerjaan*   : ' + input_job2 + '%0A' +
+        '*Isi Message*  : ' + input_message2 + '%0A';
 
     /* Whatsapp Window Open */
     window.open(blanter_whatsapp,'_blank');
